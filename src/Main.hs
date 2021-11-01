ebde4d38f3c0b36595315da10632d4f157d240ae
dm9pZCAq <46228973+dm9pZCAq@users.noreply.github.com>
2021-11-01 21:19:01 +0200

    src: Main: enable all warnings, add top-level type signature

--- src/Main.hs
+++ src/Main.hs
@@ -1,11 +1,15 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE Strict #-}
+
+{-# OPTIONS_GHC -Wall #-}
 
 
 module Main where
 
 import Network.HTTP ( getRequest, getResponseBody, simpleHTTP ) 
 
+openURL :: String -> IO String
 openURL x = getResponseBody =<< simpleHTTP (getRequest x)
 
 
