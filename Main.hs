78fdbf51fc0939dcd2a2e426e5fb4e498cb7ccf1
fulcanelly <dallasxfulcanelly@gmail.com>
2021-11-01 20:31:35 +0200

    add networking blueprint

--- Main.hs
+++ Main.hs
@@ -1,4 +1,13 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE BlockArguments #-}
+
+
 module Main where
+
+import Network.HTTP ( getRequest, getResponseBody, simpleHTTP ) 
+
+openURL x = getResponseBody =<< simpleHTTP (getRequest x)
+
 
 main :: IO ()
 main = putStrLn "Hello, Haskell!"
