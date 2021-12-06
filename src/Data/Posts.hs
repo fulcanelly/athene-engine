0b8f469bf5052b5934cc485d493da120e076a6de
fulcanelly <dallasxfulcanelly@gmail.com>
2021-12-06 20:55:28 +0200

    add method for updating post

--- src/Data/Posts.hs
+++ src/Data/Posts.hs
@@ -2,15 +2,18 @@
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE NoMonoLocalBinds #-}
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE NoMonomorphismRestriction #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module Data.Posts where
-import Database.SQLite.Simple
-import Control.Applicative
+import Database.SQLite.Simple ( execute, query, field, Only(Only), FromRow(..), Connection )
+import Control.Applicative ()
 import Database.SQLite.Simple.FromRow ( RowParser )
-import Data.Maybe (listToMaybe)
-import Data.Functor.Identity
-import Database.SQLite.Simple.FromField
-
+import Data.Functor.Identity ( Identity )
+import Database.SQLite.Simple.FromField ()
+import Data.Maybe ( fromMaybe, listToMaybe )
 
 type family AnyOrId s a where
     AnyOrId Identity a = a
@@ -23,12 +26,21 @@ data AdvPostTemplate f = Post {
     , link :: AnyOrId f String
 }
 
-
 type AdvPost = AdvPostTemplate Identity
+type PartialPost = AdvPostTemplate Maybe
 
 instance FromRow AdvPost where
     fromRow = Post <$> field <*> field <*> field <*> field
 
+sumP :: AdvPost -> PartialPost -> AdvPost
+sumP post pPost = post {
+        title = unpackM title (title post),
+        channelId = unpackM channelId (channelId post),
+        fileId = unpackM fileId (fileId post),
+        link = unpackM link (link post)
+    }
+    where unpackM field = flip fromMaybe $ field pPost
+-- can't make work: unpackM field = unpackM (field pPost) (field post)
 
 setupDB :: Connection -> IO ()
 setupDB conn =
@@ -36,6 +48,15 @@ setupDB conn =
     where query = "CREATE TABLE IF NOT EXISTS channel_posts(\
         \ title, channel_id, file_id, link, \
         \ FOREIGN KEY(channel_id) REFERENCES channel(id))"
+
+updatePost :: AdvPost -> PartialPost -> Connection -> IO ()
+updatePost post update conn = 
+    let origin = channelId post in 
+    let updater = execute conn "UPDATE channel_posts SET title = ?, channelId = ?, fileId = ?, link = ? WHERE channelId = ?" in
+        updater updateEntry summed origin
+    where 
+        updateEntry Post{..} origin = (title, channelId, fileId, link, origin)
+        summed = sumP post update
 
 createNewPost :: AdvPost -> Connection -> IO ()
 createNewPost Post{..} conn  =
