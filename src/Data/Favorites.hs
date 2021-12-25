9f57ce5075dcc39413b30f89e7afb302df2cbcc0
fulcanelly <dallasxfulcanelly@gmail.com>
2021-12-25 07:04:30 +0200

    translate facorites to free monad connection domain

--- src/Data/Favorites.hs
+++ src/Data/Favorites.hs
@@ -1,10 +1,11 @@
 {-# LANGUAGE LambdaCase #-}
 module Data.Favorites where
-import Database.SQLite.Simple 
-import API.Telegram 
+import Database.SQLite.Simple hiding (execute, query, execute_, query_)
+import API.Telegram
 import Data.Maybe
+import Control.Database
 
-data Favorite 
+data Favorite
     = Favorite {
         userId :: ChatId
         , subject :: ChatId
@@ -12,36 +13,36 @@ data Favorite
     }
 
 instance FromRow Favorite where
-    fromRow = Favorite <$> field <*> field <*> field 
+    fromRow = Favorite <$> field <*> field <*> field
 
-setupDB :: Connection -> IO ()
-setupDB conn = execute_ conn "CREATE TABLE IF NOT EXISTS channel_favorites(is_liked, subject, user_id, \
+setupDB :: SqlRequest ()
+setupDB = execute_  "CREATE TABLE IF NOT EXISTS channel_favorites(is_liked, subject, user_id, \
                 \ FOREIGN KEY (subject) REFERENCES channel_posts(user_id), \
                 \ FOREIGN KEY (user_id) REFERENCES channel_posts(user_id) \
-                \ )" 
+                \ )"
 
-likePostBy :: ChatId -> ChatId -> Connection -> IO ()
-likePostBy subject userId conn = do
-    (subject `createOrUpdateIfPresent` userId) True conn
+likePostBy :: ChatId -> ChatId -> SqlRequest ()
+likePostBy subject userId = do
+    (subject `createOrUpdateIfPresent` userId) True
 
-dislikePostBy :: ChatId -> ChatId -> Connection -> IO ()
-dislikePostBy subject userId conn = do
-    (subject `createOrUpdateIfPresent` userId) False conn
+dislikePostBy :: ChatId -> ChatId -> SqlRequest ()
+dislikePostBy subject userId = do
+    (subject `createOrUpdateIfPresent` userId) False
 
-createOrUpdateIfPresent :: ChatId -> ChatId -> Bool -> Connection -> IO ()
-createOrUpdateIfPresent subject userId state conn = do
-    res <- (subject `getRateBy` userId) conn 
-    (case res of 
+createOrUpdateIfPresent :: ChatId -> ChatId -> Bool -> SqlRequest ()
+createOrUpdateIfPresent subject userId state = do
+    res <- subject `getRateBy` userId
+    (case res of
         Nothing -> createLikeEntry
         Just fav -> updateLikeEntry
-        ) subject userId state conn  
+        ) subject userId state
 
-updateLikeEntry :: ChatId -> ChatId -> Bool -> Connection -> IO ()
-updateLikeEntry subject userId state conn = execute conn "UPDATE channel_favorites SET is_liked = ? WHERE subject = ? AND user_id = ?" (state, subject, userId)
+updateLikeEntry :: ChatId -> ChatId -> Bool -> SqlRequest ()
+updateLikeEntry subject userId state = execute "UPDATE channel_favorites SET is_liked = ? WHERE subject = ? AND user_id = ?" (state, subject, userId)
 
-createLikeEntry :: ChatId -> ChatId -> Bool -> Connection -> IO ()
-createLikeEntry subject userId state conn = (conn `execute` "INSERT INTO channel_favorites VALUES (?, ?, ?)") (state, subject, userId)
+createLikeEntry :: ChatId -> ChatId -> Bool -> SqlRequest ()
+createLikeEntry subject userId state = execute "INSERT INTO channel_favorites VALUES (?, ?, ?)" (state, subject, userId)
 
-getRateBy :: ChatId -> ChatId -> Connection -> IO (Maybe Favorite)
-getRateBy subject userId conn = 
-    listToMaybe <$> query conn "SELECT * FROM channel_favorites WHERE user_id = ?" (Only (userId :: Int))
+getRateBy :: ChatId -> ChatId -> SqlRequest (Maybe Favorite)
+getRateBy subject userId =
+    listToMaybe <$> query "SELECT * FROM channel_favorites WHERE user_id = ?" (Only (userId :: Int))
