dbd14eb8b16b4215b088693ba68bcc23f9494d3f
fulcanelly <dallasxfulcanelly@gmail.com>
2021-12-27 06:35:56 +0200

    add blueprint for notification service

--- src/Control/Notifications.hs
+++ src/Control/Notifications.hs
@@ -1,1 +1,18 @@
+{-# LANGUAGE BlockArguments #-}
 module Control.Notifications where
+import API.Telegram (ChatId)
+import Control.Concurrent.STM (newTChanIO, TChan, TVar)
+import Control.Concurrent (forkIO)
+import Control.TInter
+
+
+
+data Notification = Notify ChatId String
+
+startNotifService :: TVar ChatData -> IO (TChan Notification)
+startNotifService cdata = do
+    chan <- newTChanIO
+    forkIO do 
+        pure ()
+    pure chan
+
