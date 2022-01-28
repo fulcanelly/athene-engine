057cd009688c16243a11596bf59b8ac1546d31e7
fulcanelly <dallasxfulcanelly@gmail.com>
2022-01-29 01:52:32 +0200

    Revert "generalize some parts of restore"
    This reverts commit af2e18dbf0c99728275d1b0b590bff0a0db18dae.

--- src/Control/Restore.hs
+++ src/Control/Restore.hs
@@ -99,7 +99,7 @@ cleanAllAfterMoment chat moment = execute "DELETE FROM event_storage WHERE chat 
 cleanState :: Level -> ChatId -> SqlRequest ()
 cleanState level chat = execute "DELETE FROM event_storage WHERE chat = ? AND level >= ?" (chat, level)
 
-newtype CantRestore = CantRestore String
+newtype CantRestore = CantRestore String 
   deriving (Show, Exception)
 
 catchRestore :: IO a -> (CantRestore -> IO a) -> IO a
@@ -110,87 +110,77 @@ data Restored a = Restored {
     level_ :: Int
   }
 
+restoreScen :: [SavedEvent] -> Level -> Free ScenarioF a -> Restored a
+restoreScen [] level bot  = case bot of 
+  Free (Eval cmd next) -> restoreScen [] level next
+  Free (Clean _ next) -> restoreScen [] level next
+  _ -> Restored bot level 
 
-type Restorer p a = [SavedEvent] -> Int -> Free ScenarioF a -> p a
-
-genericRestore :: Restorer a p -> Restorer a p
-genericRestore func whole @(e : rest) level bot = case bot of
-  Pure a -> throw $ CantRestore "can't be pure"
-  Free sf -> case sf of
-    Expect f -> do
-      let (Intervened int) = e
-      case int of
-        Update up -> case f up of
-          Nothing -> throw $ CantRestore "???"
-          Just fr -> func rest level fr
-
-        AdvOffers n i -> do
-          let adjusted = onPostLike bot n
-          func rest level adjusted
-        Stop -> throw $ CantRestore "Not implemented yet"
-
-    Eval com fr -> do
-      case com of
-        SendWith me -> func rest level fr
-        _ -> func whole level fr
-
-    ReturnIf p fr fr' -> throw $ CantRestore "can't be done"
-
-    FindRandPost f ->
-      let (Posted post) = e in func rest level (f post)
-
-    LoadMyPost f -> do
-      let (Posted post) = e in func rest level (f post)
-
-    Clean level' next -> func whole level' next
-
-genericRestore _ _ _ _ = undefined
 
 -- todo: make better restoring by utilizing binary search  
 -- ie if can't restore: load 1/2 * count and try again 
 -- if can then load 1/2 + 1/4 else 1/2 - 1/4 and try restore ...
 
+restoreScen whole @(e : rest) level bot = case bot of
+  Pure a -> throw $ CantRestore "can't be pure"
+  Free sf -> case sf of 
+    Expect f -> do 
+      let (Intervened int) = e
+      case int of 
+        Update up -> case f up of
+          Nothing -> throw $ CantRestore "???"
+          Just fr -> restoreScen rest level fr 
+        AdvOffers n i -> do
+          --todo: writeIORef lastBeforeSwitch <$> readIORef lastSend
+          let adjusted = onPostLike bot n
+          restoreScen rest level adjusted
+        Stop -> throw $ CantRestore "Not implemented yet"
 
-restoreScen :: Restorer Restored a
-restoreScen list level bot
-  | list == [] =
-    case bot of
-    Free (Eval cmd next) -> restore [] level next
-    Free (Clean _ next) -> restore [] level next
-    _ -> Restored bot level
-  | otherwise = restore list level bot
-  where
-  restore = genericRestore restoreScen
+    Eval com fr -> do 
+      case com of
+        SendWith me -> restoreScen rest level fr
+        _ -> restoreScen whole level fr
+    
+    ReturnIf p fr fr' -> throw $ CantRestore "can't be done"
+    
+    FindRandPost f -> 
+      let (Posted post) = e in restoreScen rest level (f post) 
 
-restoreScenKeepingLastSent :: Restorer Restored a
-restoreScenKeepingLastSent list level bot
-  | null list = case bot of
-    Free (Clean _ next) -> restore [] level next
-    _ -> result
+    LoadMyPost f -> do
+      let (Posted post) = e in restoreScen rest level (f post) 
 
-  | all (Sent ==) list = result
-
-  | otherwise = restore list level bot
-  where
-  result = Restored bot level
-  restore = genericRestore restoreScenKeepingLastSent
+    Clean level' next -> restoreScen whole level' next
 
 
-{-
-restoreScenIO list level bot
-  | null list = case bot of
-    Free (Eval cmd next) -> restoreScenIO [] level next
-    Free (Clean _ next) -> restoreScenIO [] level next
-    _ -> pure $ Restored bot level
-  | otherwise = 
-    case list of 
-    e : rest -> do
-      putStrLn "=== got event: "
-      pPrint e
-      rest_ list level bot
-    _ -> rest_ list level bot
+restoreScenIO [] bot = case bot of 
+  Free (Eval cmd next) -> restoreScenIO [] next
+  Free (Clean _ next) -> restoreScenIO [] next
+  _ -> pure bot 
 
-  where
-  rest_ = genericRestore restoreScenIO 
 
--}
+
+restoreScenIO whole @(e : rest) bot = do
+  putStrLn "=== got event: "
+  pPrint e
+  case bot of
+    Pure a -> throw $ CantRestore "can't be pure"
+    Free sf -> case sf of 
+      Expect f -> do 
+        let (Intervened (Update u)) = e in case f u of
+          Nothing -> throw $ CantRestore "???"
+          Just fr -> restoreScenIO rest fr 
+      
+      Eval com fr -> do 
+        case com of
+          SendWith me -> restoreScenIO rest fr
+          _ -> restoreScenIO whole fr
+      
+      ReturnIf p fr fr' -> throw $ CantRestore "can't be done"
+      
+      FindRandPost f -> 
+        let (Posted post) = e in restoreScenIO rest (f post) 
+
+      LoadMyPost f -> do
+        let (Posted post) = e in restoreScenIO rest (f post) 
+
+      Clean _ next -> restoreScenIO whole next
