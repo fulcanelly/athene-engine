56685b1476bc44af3567ed5de875a4fab4493174
fulcanelly <dallasxfulcanelly@gmail.com>
2022-12-26 02:59:48 +0200

    fut: adjust models

--- bot/model/create.rb
+++ bot/model/create.rb
@@ -1,68 +1,8 @@
 
-class CreateAll < ActiveRecord::Migration[7.0]
-    def change 
+class CreateAll < CoreCreateAll
+    def change
 
-        #telegram user 
-        create_table :users, if_not_exists: true do |t|
-                    
-            t.string :name
-            t.integer :user_id
-            
-            t.timestamps 
-        end
-        
-        #state of bot
-        create_table :states, if_not_exists: true do |t|
-            t.binary :state_dump 
-
-            t.references :user, null: true, foreign_key: { to_table: :users }
-
-            t.timestamps
-        end
-    
-        # recorded action of state 
-        create_table :actions, if_not_exists: true do |t|
-            t.references :user, foreign_key: { to_table: :users }
-            t.string :name
-            t.binary :data
-            t.timestamps
-        end
-
-        # where player currently lives 
-        create_table :locations, if_not_exists: true do |t|
-            t.string :name
-            t.string :type 
-            t.timestamps 
-        end
-
-        #what player do for living
-        create_table :occupations, if_not_exists: true do |t|
-            t.string :name 
-            t.string :type 
-            t.timestamps 
-        end
-
-        #character is all about player 
-
-        create_table :characters, if_not_exists: true do |t|
-            t.string :name
-            t.integer :age
-            t.integer :karma 
-            t.integer :deaths
-            t.boolean :is_man?
-
-            t.references :user, null: true, foreign_key: { to_table: :users }
-            t.references :occupation, null: true, foreign_key: { to_table: :occupations }
-            t.references :location, null: true, foreign_key: { to_table: :locations }
-
-            t.timestamps 
-
-
-        end 
-
-        
+        super()        
     end
 end
 
-
-CreateAll.new.change
--- bot/model/mapping.rb
+++ bot/model/mapping.rb
@@ -1,1 +1,16 @@
+class User < ActiveRecord::Base
+    has_one :state
+
+    has_many :actions
+    has_many :some_items
+end
+
+
+class State < ActiveRecord::Base
+    belongs_to :user 
+end
+
+class Action < ActiveRecord::Base
+    belongs_to :user
+end
 
