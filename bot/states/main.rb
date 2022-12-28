91cdcc4634459a67bedfca2f594aefbe5109cb4c
fulcanelly <dallasxfulcanelly@gmail.com>
2022-12-28 04:30:00 +0200

    fut: add some logic to main state

--- bot/states/main.rb
+++ bot/states/main.rb
@@ -2,10 +2,19 @@ class MainMenuState < BaseState
 
     def run 
 
-        # if myself.channel.empty? then 
-        say "You need add channel"
-        switch_state AddChannelState.new
-        
+        if myself.channels.empty? then 
+            say "You need to add at least one channel to use bot"
+            switch_state AddChannelState.new
+        else 
+            suggest_it("What to do ?")
+                .option("Add channel") do
+                    switch_state AddChannelState.new
+                end
+                .option("My channels") do end
+                .option("None") do end
+                .exec
+        end
+
         # end
 
         switch_state MainMenuState.new 
