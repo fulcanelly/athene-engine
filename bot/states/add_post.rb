89c702edbbac74c154c67f6bb9e0cbaf1f4d51ee
fulcanelly <dallasxfulcanelly@gmail.com>
2023-02-05 04:17:00 +0200

    fix: typo adjustments

--- bot/states/add_post.rb
+++ bot/states/add_post.rb
@@ -1,41 +1,41 @@
 #TODO post settings (markdown, link preview)
 #TODO add ability to add links buttons
-#TODO add post preview 
+#TODO add post preview
 #TODO add ability to list/remove/edit them
 
-class AddAdvertsPostState < BaseState 
+class AddAdvertsPostState < BaseState
 
-    def restart 
+    def restart
         switch_state self.class.new
 
     end
 
-    def more_or_exit 
+    def more_or_exit
         suggest_it("Add more or continue ?")
-            .option("Contnue ?") do 
+            .option("Continue") do
                 switch_state FindPartnersState.new
             end
-            .option("Add more") do 
+            .option("Add more") do
                 restart()
             end
             .exec
     end
 
-    def make_text_post 
+    def make_text_post
         text = text_or_nil("Etner message text")
-        restart() unless text 
+        restart() unless text
         say "Post added"
         more_or_exit()
     end
 
-    def run 
+    def run
 
         suggest_it("Select type of post:")
-            .option('Text') do 
+            .option('Text') do
                 make_text_post()
             end
-            .option('GIF') do in_dev() end 
-            .option('Photo') do in_dev() end 
+            .option('GIF') do in_dev() end
+            .option('Photo') do in_dev() end
             .option('Video') do in_dev() end
             .option('Back') do
                 switch_state MainMenuState.new
@@ -43,7 +43,7 @@ class AddAdvertsPostState < BaseState
             .exec
     end
 
-    def 
+    
     def in_dev
         say "In developemnt"
         restart()
