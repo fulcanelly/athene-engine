2d99b8649c7524edad5394c282d83610227e3dda
fulcanelly <dallasxfulcanelly@gmail.com>
2023-10-03 19:47:41 +0300

    fix

--- test/all.test.ts
+++ test/all.test.ts
@@ -3,12 +3,15 @@ import { makeChannelRatingTest } from "./likes";
 import { describe, it, expect, test, beforeAll, beforeEach, afterAll} from '@jest/globals';
 import { neogma } from "../src/neo4j";
 import { makePostingTests } from "./posting";
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 describe('Athene', () => {
     makeChannelPartnersServiceTest()
     makeChannelRatingTest()
     makePostingTests()
-    
+
     afterAll(async () => {
         await neogma.driver.close()
     })
